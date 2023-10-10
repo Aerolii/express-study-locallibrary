3ae55d5af4e6ff1d843a5ae007b51090a6af7f3d
weenie89 <weenie89@Zhang.local>
2023-10-10 20:56:20 +0800

    chore: 增加数据库连接；并查询数据库数据

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,9 +1,39 @@
 const Book = require('../models/book')
+const Author = require('../models/author')
+const BookInstance = require('../models/bookinstance')
+const Genre = require('../models/genre')
 
-console.log(Book)
+const async = require('async')
 
-exports.index = (req, res) => {
-  res.send('未实现：站点首页')
+exports.index = async (req, res) => {
+  // res.send('未实现：站点首页')
+  async.parallel(
+    {
+      book_count: async function (callback) {
+        return await Book.countDocuments({}) // Pass an empty object as match condition to find all documents of this collection
+      },
+      book_instance_count: async function (callback) {
+        return await BookInstance.countDocuments({})
+      },
+      book_instance_available_count: async function (callback) {
+        return await BookInstance.countDocuments({ status: 'Available' })
+      },
+      author_count: async function (callback) {
+        return await Author.countDocuments({})
+      },
+      genre_count: async function (callback) {
+        return await Genre.countDocuments({})
+      }
+    },
+    function (err, results) {
+      console.log('results :>> ', results)
+      res.render('index', {
+        title: 'Local Library Home',
+        error: err,
+        data: results
+      })
+    }
+  )
 }
 
 exports.book_list = (req, res) => {
