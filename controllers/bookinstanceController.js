ecc5ce9889648069e30123b85045e9daffa4ec6d
weenie89 <weenie89@Zhang.local>
2023-10-10 17:14:04 +0800

    chore: 新增 书籍、书籍副本、书籍种类、作者处理路由器

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -3,33 +3,33 @@ const BookInstance = require('../models/bookinstance')
 console.log(BookInstance)
 
 exports.book_instance_list = (req, res) => {
-  res.send('未实现：查询书籍列表')
+  res.send('未实现：查询书籍副本列表')
 }
 
 exports.book_instance_detail = (req, res) => {
-  res.send('未实现：查询书籍详细信息')
+  res.send('未实现：查询书籍副本详细信息')
 }
 
 exports.book_instance_create_get = (req, res) => {
-  res.send('未实现：创建书籍表单的 GET')
+  res.send('未实现：创建书籍副本表单的 GET')
 }
 
 exports.book_instance_create_post = (req, res) => {
-  res.send('未实现：创建书籍的 POST')
+  res.send('未实现：创建书籍副本的 POST')
 }
 
 exports.book_instance_delete_get = (req, res) => {
-  res.send('未实现：删除书籍表单的 GET')
+  res.send('未实现：删除书籍副本表单的 GET')
 }
 
 exports.book_instance_delete_post = (req, res) => {
-  res.send('未实现：删除书籍的 POST')
+  res.send('未实现：删除书籍副本的 POST')
 }
 
 exports.book_instance_update_get = (req, res) => {
-  res.send('未实现：更新书籍表单的 GET')
+  res.send('未实现：更新书籍副本表单的 GET')
 }
 
 exports.book_instance_update_post = (req, res) => {
-  res.send('未实现：更新书籍信息的 POST')
+  res.send('未实现：更新书籍副本信息的 POST')
 }
